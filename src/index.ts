21e5809c794835ae74f34a7a3fc3e86c9fc6c111
DZWebflowers <contact@dzwebflowers.com>
2023-09-16 13:46:05 +0200

    Main changes introduced.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { greetUser } from '$utils/greet';
+import { initializeCardAnimation } from 'src/cards/Cards';
+import { values } from 'src/Values';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
+const linkElement = document.createElement('link');
+linkElement.rel = 'stylesheet';
+linkElement.href = '../src/index.css';
+
+document.head.appendChild(linkElement);
+
+initializeCardAnimation();
+values();
